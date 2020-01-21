726cdac1d14e98359d4e409a012403f012b15152
Dirk Helfenstein <Dirk.Helfenstein@fadata.eu>
2020-01-21 12:49:33 +0000

    Merged PR 1536: new ice version
    - updated the ice foundation packages
    - create an ice runtime art start
    
    Related work items: #7838

--- apps/angular/src/app/components/app/app.component.ts
+++ apps/angular/src/app/components/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { IceRuntimeService } from '@impeo/ng-ice';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  //
+  //
+  constructor(private iceRuntimeService: IceRuntimeService) {
+    this.iceRuntimeService.getRuntime();
+  }
+}
