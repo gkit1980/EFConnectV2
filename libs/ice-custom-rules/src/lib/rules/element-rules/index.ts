ae2b489b005629faee6e7115a9a36aa6f0f57f58
Marija Jelenkovic <Marija.Jelenkovic@fadata.eu>
2020-02-26 09:25:07 +0000

    Merged PR 1709: Property quote&apply: create navigation and pages
    Related work items: #8359

--- libs/ice-custom-rules/src/lib/rules/element-rules/index.ts
+++ libs/ice-custom-rules/src/lib/rules/element-rules/index.ts
@@ -1,2 +1,3 @@
 export * from './view-mode/dummy-view-mode-rule-one';
 export * from './default-value/array-item-index-value-rule/array-item-index-value-rule';
+export * from './default-value/element-value-rule/element-value-rule';
