2b2945e257cbbe47d5172cf822605a874543d159
Dirk Helfenstein <Dirk.Helfenstein@fadata.eu>
2020-02-28 10:45:13 +0000

    Merged PR 1721: loading iban for policyholder
    - loading accounts for client
    - using new valuesrule for displaying the items
    
    Related work items: #8393

--- libs/ice-custom-rules/src/lib/rules/element-rules/index.ts
+++ libs/ice-custom-rules/src/lib/rules/element-rules/index.ts
@@ -1,3 +1,4 @@
 export * from './view-mode/dummy-view-mode-rule-one';
 export * from './default-value/array-item-index-value-rule/array-item-index-value-rule';
 export * from './default-value/element-value-rule/element-value-rule';
+export * from './values/array-item-based-values-rule';
