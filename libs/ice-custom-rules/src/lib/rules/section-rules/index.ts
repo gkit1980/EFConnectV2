ae6815eefd2f8a9c2b3ec8d352ac10f373b3da30
Lyuboslav Lyubenov <lyuboslav.lyubenov@fadata.eu>
2020-06-03 05:38:10 +0000

    Merged PR 1909: Show no active policies if there arent any active policies and hide show expired policies if there arent any expired policies
    Related work items: #8944

--- libs/ice-custom-rules/src/lib/rules/section-rules/index.ts
+++ libs/ice-custom-rules/src/lib/rules/section-rules/index.ts
@@ -1,1 +1,2 @@
 export * from './view-mode/hide-section-when-array-element-is-empty';
+export * from './view-mode/change-view-mode-based-on-array-length-rule/change-view-mode-based-on-array-length-rule';
---
+++ libs/ice-custom-rules/src/lib/rules/section-rules/view-mode/change-view-mode-based-on-array-length-rule/change-view-mode-based-on-array-length-rule.ts
@@ -0,0 +1,69 @@
+import { SectionViewModeRule, SectionViewMode, IceConsole, ArrayElement } from '@impeo/ice-core';
+import { get } from 'lodash';
+
+export enum Conditions {
+  LESS_THAN = 'leser',
+  EQUALS = 'equals',
+  GREATER_THAN = 'greater'
+}
+
+export class ChangeViewModeBasedOnArrayLengthRule extends SectionViewModeRule {
+  getViewMode(actionContext?: any): SectionViewMode {
+    const array = this.requireElement('array');
+    const condition = this.requireParam('condition');
+    const conditionValue = parseInt(this.requireParam('conditionValue'), 10);
+    const viewMode = this.requireParam('viewMode');
+    let elseViewMode = this.getParam('elseViewMode', SectionViewMode.DEFAULT);
+
+    if (array.type !== 'array') {
+      IceConsole.warn(
+        `${ChangeViewModeBasedOnArrayLengthRule.name} specified element is not of type array`
+      );
+      return SectionViewMode.DEFAULT;
+    }
+
+    if (!condition || !Object.values(Conditions).includes(condition)) {
+      IceConsole.warn(
+        `${ChangeViewModeBasedOnArrayLengthRule.name} condition param is not set or is not a correct condition value (lesser, equals, greater).`
+      );
+      return SectionViewMode.DEFAULT;
+    }
+
+    if (conditionValue === NaN) {
+      IceConsole.warn(
+        `${ChangeViewModeBasedOnArrayLengthRule.name} conditionValue is not set or is not an integer.`
+      );
+      return SectionViewMode.DEFAULT;
+    }
+
+    if (!viewMode || viewMode.trim() === '' || !Object.values(SectionViewMode).includes(viewMode)) {
+      IceConsole.warn(
+        `${ChangeViewModeBasedOnArrayLengthRule.name} viewmode param is not set or is invalid.`
+      );
+      return SectionViewMode.DEFAULT;
+    }
+
+    if (elseViewMode && !Object.values(SectionViewMode).includes(elseViewMode)) {
+      IceConsole.warn(
+        `${ChangeViewModeBasedOnArrayLengthRule.name} elseViewMode is invalid viewmode. Fallsback on default view mode if length is different`
+      );
+      elseViewMode = SectionViewMode.DEFAULT;
+    }
+
+    const arrayLength = get(
+      (array as ArrayElement).getValue(),
+      ['values', 0, 'value', 'length'],
+      0
+    );
+    const equals =
+      condition.toLowerCase() === 'equals' &&
+      ((arrayLength === conditionValue && viewMode) || elseViewMode);
+    const lesser =
+      condition.toLowerCase() === 'lesser' &&
+      ((arrayLength < conditionValue && viewMode) || elseViewMode);
+    const greater =
+      condition.toLowerCase() === 'greater' &&
+      ((arrayLength > conditionValue && viewMode) || elseViewMode);
+    return equals || lesser || greater;
+  }
+}
