c69bba529baaa97a9d0b4befa15706ed03e1bd11
Stoyan Delev <stoyan.delev@fadata.eu>
2020-07-07 11:45:07 +0000

    Merged PR 2032: Fill the countries for all loaded users
    <!--
    
    Replace this comment with a summary of the change with relevant motivation and context.
    
    * Have you added unit tests that prove your fix is effective?
    * Have you rebased your code against the most recent `master`?
    * Have you performed a self-review of your own code?
    * Have you commented your code where necessary, particularly in hard-to-understand areas?
    
    -->
    
    Related work items: #10339

---
+++ ice-repo/data/get-client-by-manid.sql
@@ -0,0 +1,21 @@
+SELECT 
+    p.GNAME, 
+    p.FNAME, 
+    p.BIRTH_DATE, 
+    p.SEX,
+    p.egn as pid, 
+    MAX(a.COUNTRY_CODE) as country_code,
+    MAX(a.ADDRESS) as ADDRESS,
+    MAX(c1.DETAILS) AS phone,
+    MAX(c2.DETAILS) AS email
+FROM p_people p
+LEFT JOIN p_address a ON a.man_id = p.man_id
+LEFT JOIN p_contacts c1 ON c1.man_id = p.man_id AND c1.contact_type = 'MOBILE' and c1.primary_flag = 'Y'
+LEFT JOIN p_contacts c2 ON c2.man_id = p.man_id AND c2.contact_type = 'EMAIL' and c2.primary_flag = 'Y'
+WHERE p.man_id = :manid
+GROUP BY p.GNAME, 
+    p.FNAME, 
+    p.BIRTH_DATE, 
+    p.SEX,
+    p.egn
+FETCH first 1 ROWS ONLY 
