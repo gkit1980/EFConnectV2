afaddd940653a5315628b56b345997bdd326df60
Lyuboslav Lyubenov <lyuboslav.lyubenov@fadata.eu>
2020-07-10 11:01:33 +0000

    Merged PR 2056: make finalize animation for life
    Related work items: #10366

--- libs/ice-custom-components/src/lib/components/page-components/insis-confirmation-page/insis-confirmation-page.component.ts
+++ libs/ice-custom-components/src/lib/components/page-components/insis-confirmation-page/insis-confirmation-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PageComponentImplementation } from '@impeo/ng-ice';
+import { get } from 'lodash';
 
 @Component({
   selector: 'insis-confirmation-page',
@@ -7,4 +8,15 @@ import { PageComponentImplementation } from '@impeo/ng-ice';
 })
 export class InsisConfirmationPageComponent extends PageComponentImplementation implements OnInit {
   static componentName = 'InsisConfirmationPage';
+
+  product: string;
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.product = get(
+      this.recipe,
+      ['component', InsisConfirmationPageComponent.componentName, 'product'],
+      'motor'
+    );
+  }
 }
