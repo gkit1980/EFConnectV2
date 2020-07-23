022bc12d13cfa3a61132e36395916179b615b864
Marija Jelenkovic <Marija.Jelenkovic@fadata.eu>
2020-07-23 14:04:32 +0000

    Merged PR 2092: Dashboard definitions, refactoring ICE configuration
    Related work items: #10561, #10568, #10575, #10576, #10577, #10578

--- ice-repo/data/search-policies.sql
+++ ice-repo/data/search-policies.sql
@@ -1,40 +1,43 @@
 SELECT JSON_OBJECT(
-              'policy_state' is policy_state, 
-              'policy_id' is policy_id, 
+              'policy_state' is policy_state,
+              'policy_id' is policy_id,
               'policy_no' is policy_no,
               'policy_name' IS policy_name,
-              'insr_type' is insr_type, 
+              'insr_type' is insr_type,
               'product_name' is product_name,
               'package_code' IS package_code,
               'is_expired' IS is_expired,
-              'policy_state_name' is policy_state_name, 
+              'policy_state_name' is policy_state_name,
               'client_id' IS client_id,
               'pid' is pid,
+              'total' is datacount,
               'man_id' IS man_id,
-              'client_name' is client_name, 
-              'insr_begin' is insr_begin, 
+              'client_name' is client_name,
+              'insr_begin' is insr_begin,
               'insr_end' is insr_end,
               'premium_amnt' IS premium_amnt,
-              'active_claims' is active_claims, 
-              'engagement_id' is engagement_id, 
-              'engagement_no' is engagement_no, 
-              'engagement_name' is engagement_name, 
-              'quote_id' is quote_id, 
-              'quote_no' is quote_no, 
-              'quote_status' is quote_status, 
+              'active_claims' is active_claims,
+              'engagement_id' is engagement_id,
+              'engagement_no' is engagement_no,
+              'engagement_name' is engagement_name,
+              'quote_id' is quote_id,
+              'quote_no' is quote_no,
+              'quote_status' is quote_status,
               'quote_status_name' is quote_status_name,
               'insured_objects' is (
-                    SELECT JSON_ARRAYAGG( JSON_OBJECT('object_id' is io.object_id, 'object_name' is object_name) ) 
+                    SELECT JSON_ARRAYAGG( JSON_OBJECT('object_id' is io.object_id, 'object_name' is object_name) )
                       FROM insured_object io
                           LEFT JOIN insured_object_names ion
                             ON ion.insured_obj_id = io.insured_obj_id
                         WHERE io.policy_id = policy_id_for_object
+                          AND ((:p_insured_object IS NULL) OR (object_name LIKE :p_insured_object))
                           AND open_date >= io.insr_begin
-                          AND open_date <= io.insr_end ) )
-          FROM ( 
+                          AND open_date <= io.insr_end )  )
+          FROM (
         WITH od AS (SELECT sys_days.get_open_date AS open_date FROM dual)
         SELECT pol.policy_state, pol.policy_id, pol.policy_id AS policy_id_for_object, pol.policy_no, pol.policy_name, pol.insr_type,
               hit.name AS product_name,
+              ceil(count(*) over ()) as datacount,
               (SELECT package_type
                   FROM ( SELECT package_type, rank() over (ORDER BY PACKAGE_id desc) AS rnk
                           FROM gen_packages GP
@@ -50,13 +53,13 @@ SELECT JSON_OBJECT(
               LTRIM(TO_CHAR( (SELECT SUM(amount)
                                   FROM blc_policy_payment_plan
                                 WHERE policy_id = pol.policy_id
-                                  AND amnt_type = 'DUE' ), 9999990.99 )) AS premium_amnt,  
+                                  AND amnt_type = 'DUE' ), 9999990.99 )) AS premium_amnt,
              ( SELECT NVL( SUM( DECODE( clm.claim_state, -1, 1, 1, 1, 0)), 0 )
                  FROM ( SELECT c.claim_id, MIN(cr.claim_state) AS claim_state
                           FROM insis_gen_v10.claim c
                                     INNER JOIN insis_gen_v10.claim_request cr ON c.claim_id = cr.claim_id
                          WHERE c.policy_id = pol.policy_id
-                         GROUP BY c.claim_id ) clm ) AS active_claims, pe.engagement_id, pe.engagement_no, pe.engagement_name, 
+                         GROUP BY c.claim_id ) clm ) AS active_claims, pe.engagement_id, pe.engagement_no, pe.engagement_name,
               peq.quote_id, peq.quote_no, peq.status as quote_status, hsv.NAME AS quote_status_name, od.open_date
           FROM policy pol
         CROSS JOIN od
@@ -71,18 +74,32 @@ SELECT JSON_OBJECT(
           LEFT JOIN policy_engagement_quote peq
             ON peq.quote_id = pep.quote_id
           LEFT JOIN hst_status_value hsv
-            ON hsv.id = peq.status 
+            ON hsv.id = peq.status
         WHERE pol.policy_state <> -10
           AND pol.policy_id LIKE NVL(:policy_id, pol.policy_id)
             AND pol.policy_no LIKE NVL(:policy_no, pol.policy_no)
             AND pol.policy_name LIKE NVL(:policy_name, pol.policy_name)
+            AND pn.client_pid LIKE NVL(:p_client_pid, pn.client_pid)
+            AND pn.client_name LIKE NVL(:p_client_name, pn.client_name)
             AND pol.insr_type = NVL(:insr_type, pol.insr_type)
             AND (:policy_state IS NULL
                   OR :policy_state = CASE WHEN pol.policy_state = -4 THEN 'Quotation'
                                           WHEN pol.policy_state IN (-3, -2, -1) THEN 'Application'
                                           WHEN pol.policy_state IN (-30) THEN 'Canceled'
                                           ELSE 'Policy' END)
-            AND (:p_client IS NULL OR pn.client_pid = :p_client OR pn.client_name LIKE :p_client)
+            AND ((:p_fulltext IS NULL) OR (
+              pol.policy_id LIKE :p_fulltext OR
+              pol.policy_no LIKE :p_fulltext OR
+              pol.policy_name LIKE :p_fulltext OR
+              pol.insr_type LIKE :p_fulltext OR
+              pn.client_pid LIKE :p_fulltext OR
+              pn.client_name LIKE :p_fulltext OR
+              :p_fulltext = CASE WHEN pol.policy_state = -4 THEN 'Quotation'
+                                          WHEN pol.policy_state IN (-3, -2, -1) THEN 'Application'
+                                          WHEN pol.policy_state IN (-30) THEN 'Canceled'
+                                          ELSE 'Policy' END
+            ))
             AND (:p_insr_end IS NULL OR pol.insr_end < to_date( :p_insr_end, 'yyyy-MM-dd' ) + 1)
           ORDER BY policy_id DESC
-          FETCH first 20 ROWS ONLY )
+          OFFSET :offset ROWS
+          FETCH first :total ROWS ONLY )
