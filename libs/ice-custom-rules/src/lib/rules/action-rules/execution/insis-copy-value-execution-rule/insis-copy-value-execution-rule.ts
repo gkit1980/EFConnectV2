ef8217883da59421a806b0f00026b271fb7b0563
Lyubomira Lyubomirova <lyubomira.lyubomirova@fadata.eu>
2020-08-10 12:50:21 +0000

    Merged PR 2153: updated contacts to have most resent design
    updated contacts to have most resent design
    
    Related work items: #10782

--- libs/ice-custom-rules/src/lib/rules/action-rules/execution/insis-copy-value-execution-rule/insis-copy-value-execution-rule.ts
+++ libs/ice-custom-rules/src/lib/rules/action-rules/execution/insis-copy-value-execution-rule/insis-copy-value-execution-rule.ts
@@ -1,18 +1,43 @@
-import { ExecutionRule, IndexedValue, ValueOrigin } from '@impeo/ice-core';
+import { ExecutionRule, IndexedValue, ValueOrigin, IceElement } from '@impeo/ice-core';
 
 //
 //
 export class InsisCopyValueExecutionRule extends ExecutionRule {
+  destElement: IceElement;
   //
   //
   async execute(actionContext?: any): Promise<any> {
     const sourceElement = this.requireElement('source');
-    const destElement = this.requireElement('destination');
+    this.destElement = this.requireElement('destination');
+
     const index = actionContext != null && actionContext.index != null ? actionContext.index : null;
+
     const sourceValue = sourceElement.getValue().forIndex([index]);
-    const destIndex = IndexedValue.sliceIndexToElementLevel(destElement.name, index);
-    destElement.setValue(
-      new IndexedValue(destElement, sourceValue, destIndex, ValueOrigin.DATAMODEL)
+
+    const destIndex = this.getDestinationIndex(index);
+
+    this.destElement.setValue(
+      new IndexedValue(this.destElement, sourceValue, destIndex, ValueOrigin.DATAMODEL)
     );
+  }
+
+  getDestinationIndex(index): number[] | null {
+    const destinationIndexFormRecipe = this.getParam('destinationIndex');
+
+    let destinationindex;
+
+    destinationindex = String(destinationIndexFormRecipe)
+      .split(',')
+      .map((_index) => Number.parseInt(_index, 10))
+      .filter((_index) => !isNaN(_index));
+
+    destinationindex = destinationindex.length === 0 ? index : destinationindex;
+
+    if (!destinationindex && this.destElement.isArrayItem()) {
+      destinationindex = this.destElement.getValue().values.length - 1;
+      destinationindex = [destinationindex];
+    }
+
+    return IndexedValue.sliceIndexToElementLevel(this.destElement.name, destinationindex);
   }
 }
