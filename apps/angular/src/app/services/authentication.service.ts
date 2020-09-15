0b448135444d8cd4cd7e4e17f09f2ed06c167519
Mark Munte <Mark.Munte@fadata.eu>
2020-09-15 09:59:33 +0000

    Merged PR 2266: Change ice principal load-store
    Small changes in way ICE principal is created, now changing the principal ID on login/out, allowing the ice runtime to "clear" any loaded ice context
    
    Related work items: #10985

--- apps/angular/src/app/services/authentication.service.ts
+++ apps/angular/src/app/services/authentication.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { IcePrincipalService } from '@impeo/ng-ice';
+import { IcePrincipal, ClientPrincipal } from '@impeo/ice-core';
+import * as _ from 'lodash';
+
 import * as axios from 'axios';
 import { get } from 'lodash';
+import { getDefaultLanguage } from './language.service';
 
 type Role = 'customer' | 'agent' | null;
+const localStoragekey = 'insis-principal2';
+const nonLoggedInPrincipalId = 'n/a';
 
 @Injectable()
 export class AuthenticationService {
   get loggedIn(): boolean {
-    if (this.icePrincipalService.principal.data && this.icePrincipalService.principal.data.pid)
+    if (
+      this.icePrincipalService.principal &&
+      this.icePrincipalService.principal.id !== nonLoggedInPrincipalId
+    )
       return true;
 
     return false;
@@ -30,39 +39,64 @@ export class AuthenticationService {
   }
 
   get role(): Role {
-    return get(this.icePrincipalService.principal, 'data.role', null);
+    const role = _.first(this.icePrincipalService.principal.roles);
+    return role as Role;
   }
 
   constructor(private icePrincipalService: IcePrincipalService) {
-    this.setPrincipalDataFromStorage();
+    const principal = this.loadPrincipalFromLocalStorage();
+    if (principal) this.icePrincipalService.principal = principal;
+
+    //can we move the app.module ICE principal creation in here?
+    //maybe in absence of a principal in local storage, even return a default "dummy" principal
   }
 
-  async login(userData: any, role: string): Promise<void> {
+  async login(id: string, role: string): Promise<void> {
     try {
       const response = await axios.default.get(
-        role === 'customer'
-          ? `/api/v1/external/client/${userData}`
-          : `/api/v1/external/agent/${userData}`,
+        role === 'customer' ? `/api/v1/external/client/${id}` : `/api/v1/external/agent/${id}`,
         {
           timeout: 4000,
         }
       );
+      const currPrincipal = this.icePrincipalService.principal;
+      const newPrincipal = new ClientPrincipal(
+        response.data.pid,
+        currPrincipal.locale,
+        [role],
+        response.data
+      );
 
-      response.data['role'] = role;
-
-      localStorage.setItem('insis-principal', JSON.stringify(response.data));
-      this.icePrincipalService.principal.data = response.data;
+      this.icePrincipalService.principal = newPrincipal;
+      this.storePrincipalToLocalStorage(newPrincipal);
     } catch (error) {}
   }
 
   async logout(): Promise<void> {
-    this.icePrincipalService.principal.data = {};
+    const currPrincipal = this.icePrincipalService.principal;
+    const newPrincipal = new ClientPrincipal(nonLoggedInPrincipalId, currPrincipal.locale, [], {});
 
-    localStorage.removeItem('insis-principal');
+    this.icePrincipalService.principal = newPrincipal;
+    localStorage.removeItem(localStoragekey);
   }
 
-  private setPrincipalDataFromStorage(): void {
-    if (!this.icePrincipalService.principal.data || !this.icePrincipalService.principal.data.pid)
-      this.icePrincipalService.principal.data = JSON.parse(localStorage.getItem('insis-principal'));
+  private storePrincipalToLocalStorage(principal: IcePrincipal) {
+    const data = {
+      id: principal.id,
+      roles: principal.roles,
+      data: principal.data,
+      locale: principal.locale,
+    };
+    localStorage.setItem(localStoragekey, JSON.stringify(data));
+  }
+
+  private loadPrincipalFromLocalStorage(): IcePrincipal {
+    try {
+      const data = JSON.parse(localStorage.getItem(localStoragekey));
+      const principal = new ClientPrincipal(data.id, data.locale, data.roles, data.data);
+      return principal;
+    } catch (error) {
+      return null;
+    }
   }
 }
