4a7f74d137603a5989af0e82ceecbd80141b1f92
gkit <gkit1980@yahoo.com>
2021-12-10 20:00:43 +0200

    change paginated quey

--- ice-repo/data/funds.data.sql
+++ ice-repo/data/funds.data.sql
@@ -1,16 +1,25 @@
-SELECT json_object( 'FundsNames' is (SELECT JSON_ARRAYAGG(ul_funds.fund_name) fund_name
-                                        FROM ul_funds ),
+WITH cte
+AS
+(
+SELECT /*+MATERIALIZE*/ 
+       DISTINCT
+     to_date(trunc(price_date),'dd/mm/yyyy')price_date
+       FROM ul_unit_prices 
+        WHERE :price_date is null or price_date >= TO_DATE(:price_date,'dd/mm/yyyy')  AND PRICE_TYPE='SELL'
+        order by  to_date(trunc(price_date),'dd/mm/yyyy') asc
+)
+SELECT json_object( 'FundsNames' is (SELECT JSON_ARRAYAGG(ul_funds.fund_name) fund_name FROM ul_funds ),
+                    'periods' is (select json_arrayagg(price_date order by price_date asc) from cte),
                     'Funds' is (SELECT JSON_ARRAYAGG
-                                    (JSON_OBJECT('fundname' IS (SELECT FUND_NAME FROM ul_funds S WHERE B.fund_id=S.FUND_ID),
-                                               'periods' IS JSON_ARRAYAGG(to_date(trunc(price_date),'dd/mm/yyyy') order by price_date asc RETURNING CLOB),                                             
-                                                 'Values' IS JSON_ARRAYAGG(price order by price_date RETURNING CLOB)
-                                    )RETURNING CLOB)
+                                    (
+                                        JSON_OBJECT('fundname' IS (SELECT FUND_NAME FROM ul_funds S WHERE B.fund_id=S.FUND_ID),
+                                                    'Values' IS JSON_ARRAYAGG(price order by price_date RETURNING CLOB)
+                                                    )RETURNING CLOB
+                                    )
                                 FROM  ul_unit_prices B
                                 WHERE :price_date is null or price_date >= TO_DATE(:price_date,'dd/mm/yyyy')  AND PRICE_TYPE='SELL' 
                                 GROUP BY  fund_id
-                                OFFSET 0 ROWS FETCH FIRST 10 ROWS ONLY
-                                ) 
-                                RETURNING CLOB 
+                                )RETURNING CLOB 
                     )
 FROM dual;
 
